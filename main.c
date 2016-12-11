da284a1f819d06ce9595060b0d01b369430ad921
GMouaad <Mgsair@gmail.com>
2016-12-11 01:57:38 +0100

    C Projekt : BMI

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 11, 2016
+ *      Author: MouaadGssair
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//prototypes
+
+int main(void)
+{
+	
+	
+	printf("\tHello\n");
+	
+	return 0;
+}
---
+++ prototypes.h
@@ -0,0 +1,13 @@
+/*
+ * prototypes.h
+ *
+ *  Created on: Dec 11, 2016
+ *      Author: MouaadGssair
+ */
+
+#ifndef PROTOTYPES_H_
+#define PROTOTYPES_H_
+
+
+
+#endif /* PROTOTYPES_H_ */
